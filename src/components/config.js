b20becb8ac50ef0ee3db52e32c4754ba38ba8711
Pavel Belopolsky <pavelbelopolsky@MacBook-Air-Pavel-2.local>
2024-12-20 18:10:43 +0300

    ready

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,6 @@
 
 export const config = {
-  baseUrl: "https://nomoreparties.co/v1/frontend-st-cohort-202",
+  baseUrl: "https://nomoreparties.co/v1/frontend-st-cohort-201",
   headers: {
       authorization: 'c917b3f9-c486-401a-a659-88cbab8f8837',
       "Content-Type": "application/json"
